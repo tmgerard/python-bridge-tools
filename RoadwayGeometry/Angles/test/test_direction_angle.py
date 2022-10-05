1f2b3fc7337532499aa1f1e737dfa7494d7e94e9
tmgerard <tom.gerard@gmail.com>
2022-10-05 13:10:22 -0500

    Modified unit tests with the correct expected bearing angle format

--- RoadwayGeometry/Angles/test/test_direction_angle.py
+++ RoadwayGeometry/Angles/test/test_direction_angle.py
@@ -7,22 +7,22 @@ from RoadwayGeometry.Angles.direction_angle import DirectionAngle
 class TestDirectionAngle(unittest.TestCase):
 
     def test_forty_five_degree_northeast_bearing(self):
-        expected = "N 45° 00' 0.00\" E"
+        expected = "N 45° 00' 00\" E"
         angle = DirectionAngle(math.pi / 4.0)
         self.assertEqual(expected, angle.to_bearing())
 
     def test_forty_five_degree_northwest_bearing(self):
-        expected = "N 45° 00' 0.00\" W"
+        expected = "N 45° 00' 00\" W"
         angle = DirectionAngle(3 * math.pi / 4.0)
         self.assertEqual(expected, angle.to_bearing())
 
     def test_forty_five_degree_southwest_bearing(self):
-        expected = "S 45° 00' 0.00\" W"
+        expected = "S 45° 00' 00\" W"
         angle = DirectionAngle(5 * math.pi / 4.0)
         self.assertEqual(expected, angle.to_bearing())
 
     def test_forty_five_degree_southeast_bearing(self):
-        expected = "S 45° 00' 0.00\" E"
+        expected = "S 45° 00' 00\" E"
         angle = DirectionAngle(7 * math.pi / 4.0)
         self.assertEqual(expected, angle.to_bearing())
 
