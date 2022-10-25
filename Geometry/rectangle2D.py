a43e2d2adc9536265f08fb1a44c2777ab1fd0f67
tmgerard <tom.gerard@gmail.com>
2022-10-25 12:05:36 -0500

    Added coordinate point to Rectangle2D

--- Geometry/rectangle2D.py
+++ Geometry/rectangle2D.py
@@ -1,10 +1,15 @@
 import math
 
+from Geometry.point2D import Point2D
+from Geometry.num_compare import effectively_equal
+
 
 class Rectangle2D:
-    
-    # TODO: add point2D class to define location by coordates and eventually draw shapes
-    def __init__(self, width: float, height: float):
+    """
+    Represents a rectangle with a given width, height, and coordinate position
+    """
+
+    def __init__(self, width: float, height: float, origin=Point2D(0, 0)):
         if width <= 0:
             raise ValueError("Rectangle width must be a positive value")
 
@@ -13,21 +18,70 @@ class Rectangle2D:
 
         self.width = width
         self.height = height
+        self.origin = origin  # bottom left corner of rectangle
 
     @property
-    def area(self):
+    def area(self) -> float:
         """
         Area of rectangle
         """
         return self.width * self.height
 
     @property
-    def diagonal_length(self):
+    def bottom(self) -> float:
+        """
+        Returns y-coordinate of the rectangle's bottom bottom edge
+        """
+        return self.origin.y
+
+    def contains_point(self, point: Point2D) -> bool:
+        """
+        Checks if a given Point2D lies within the rectangle
+        """
+        return self.left < point.x < self.right and self.bottom < point.y < self.top
+
+    @property
+    def diagonal_length(self) -> float:
         """
         Distance bottom corner to the opposite side top corner of the rectangle
         """
         return math.sqrt(self.width ** 2 + self.height ** 2)
 
     @property
-    def perimeter(self):
+    def left(self) -> float:
+        """
+        Returns x-coordinate of the rectangle's left edge
+        """
+        return self.origin.x
+
+    @property
+    def perimeter(self) -> float:
+        """
+        Returns the rectangle's perimeter length
+        """
         return 2.0 * (self.width + self.height)
+
+    @property
+    def right(self) -> float:
+        """
+        Returns the x-coordinate of the rectangle's right edge
+        """
+        return self.origin.x + self.width
+
+    @property
+    def top(self) -> float:
+        """
+        Returns the y-coordinate of the rectangle's top edge
+        """
+        return self.origin.y + self.height
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+
+        if not isinstance(other, Rectangle2D):
+            return False
+
+        return self.origin == other.origin and \
+            effectively_equal(self.width, other.width) and \
+            effectively_equal(self.height, other.height)
