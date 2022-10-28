9a9c7fc7c95108bee06baabec4cb77c130f87820
tmgerard <tom.gerard@gmail.com>
2022-10-28 11:29:38 -0500

    Added Vector3D related method and operator overload

--- BridgeMath/Geometry/point3D.py
+++ BridgeMath/Geometry/point3D.py
@@ -1,6 +1,7 @@
 import math
 
 from BridgeMath.num_compare import effectively_equal
+from BridgeMath.Geometry.vector3D import Vector3D
 
 
 class Point3D:
@@ -13,6 +14,20 @@ class Point3D:
         self.y = y
         self.z = z
 
+    def displaced(self, vector: Vector3D, times=1) -> 'Point3D':
+        """
+        Returns Point3D that is the current point displaced by a given Vector3D a specified number of times
+        :param vector: Vector to displace point by
+        :param times: Number of times to displace point by the given vector length
+        :return: Point3D
+        """
+        scaled_vec = vector.scaled_by(times)
+        return Point3D(
+            self.x + scaled_vec.u,
+            self.y + scaled_vec.v,
+            self.z + scaled_vec.w
+        )
+
     def distance_to(self, other: 'Point3D') -> float:
         """
         Calculates the distance to a given Point3D
@@ -23,6 +38,13 @@ class Point3D:
         delta_y = other.y - self.y
         delta_z = other.z - self.z
         return math.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
+
+    def __sub__(self, other: 'Point3D') -> Vector3D:
+        return Vector3D(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z
+        )
 
     def __eq__(self, other):
         if self is other:
