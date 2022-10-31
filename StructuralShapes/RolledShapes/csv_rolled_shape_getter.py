9e07181e8cde3f7ebbafba0faea420f0ef43c05b
tmgerard <tom.gerard@gmail.com>
2022-10-31 11:22:59 -0500

    Added csv_rolled_shape_getter.py

---
+++ StructuralShapes/RolledShapes/csv_rolled_shape_getter.py
@@ -0,0 +1,59 @@
+from typing import Dict
+
+from StructuralShapes.RolledShapes.rolled_shape import AISC_TYPE, AISC_EDI_STD_NOMENCLATURE, AISC_MANUAL_LABEL, AISC_T_F
+
+
+class CSVRolledShapeGetterAISC15:
+    """
+    Retrieves AISC Steel Construction Manual, 15th Edition rolled shape data from CSV formatted file
+    """
+    def __init__(self, file_path: str):
+        self.__file_path = file_path
+
+    def get_data(self, shape_name: str) -> Dict:
+        """
+        Gets the rolled shape data from the CSV formatted file
+        :param shape_name: Shape name that matches the EDI_Standard_Nomenclature name from AISC Shapes Database
+        :return: Dictionary containing all shape data
+        """
+        return self.__get_csv_data(shape_name)
+
+    def __get_csv_data(self, shape_name: str) -> Dict:
+        csv_separator = ','
+        header = []
+        data = []
+
+        with open(self.__file_path, 'r') as f:
+            while True:
+                line = f.readline()
+                split = line.split(csv_separator)
+                if split[0] == AISC_TYPE:   # header row
+                    header = split
+                else:   # data row
+                    if shape_name in split:
+                        data = split
+                        break
+
+        if not data:
+            ValueError('The requested shape was not found in the AISC Shapes Database')
+
+        properties = self.__build_properties_dictionary(data, header)
+
+        return properties
+
+    def __build_properties_dictionary(self, data, header) -> Dict:
+        csv_no_data = '-'
+        properties = {}
+
+        for i in range(len(data)):
+            if not data[i] == csv_no_data:
+                # Most values in table should be floats, but there are a few that should be strings
+                if header[i] == AISC_TYPE or \
+                        header[i] == AISC_EDI_STD_NOMENCLATURE or \
+                        header[i] == AISC_MANUAL_LABEL or \
+                        header[i] == AISC_T_F:
+                    properties[header[i]] = str(data[i])
+                else:
+                    properties[header[i]] = float(data[i])
+
+        return properties
