0e69be8a35a1634b6c69834514ef863da1dd8fb6
tmgerard <tom.gerard@gmail.com>
2022-11-01 09:32:28 -0500

    Adjusted typing

--- StructuralShapes/RolledShapes/channel_shape.py
+++ StructuralShapes/RolledShapes/channel_shape.py
@@ -9,91 +9,91 @@ class ChannelShape(RolledShape):
         super().__init__(properties)
 
     @property
-    def cw(self):
+    def cw(self) -> float:
         """
         Warping constant, in^6
         """
         return self.properties[AISC_CW]
 
     @property
-    def depth(self):
+    def depth(self) -> float:
         """
         Overall depth of member, in.
         """
         return self.properties[AISC_DEPTH]
 
     @property
-    def detailing_depth(self):
+    def detailing_depth(self) -> float:
         """
         Detailing depth of member, in.
         """
         return self.properties[AISC_DETAILING_DEPTH]
 
     @property
-    def detailing_flange_thickness(self):
+    def detailing_flange_thickness(self) -> float:
         """
         Detailing flange thickness, in.
         """
         return self.properties[AISC_DETAILING_FLANGE_THICKNESS]
 
     @property
-    def detailing_flange_width(self):
+    def detailing_flange_width(self) -> float:
         """
         Detailing flange width, in.
         """
         return self.properties[AISC_DETAILING_FLANGE_WIDTH]
 
     @property
-    def detailing_k(self):
+    def detailing_k(self) -> float:
         """
         Detailing distance from outer face of flange to web toe of fillet, in.
         """
         return self.properties[AISC_K_DETAILING]
 
     @property
-    def detailing_web_thickness(self):
+    def detailing_web_thickness(self) -> float:
         """
         Detailing web thickness, in.
         """
         return self.properties[AISC_DETAILING_WEB_THICKNESS]
 
     @property
-    def flange_thickness(self):
+    def flange_thickness(self) -> float:
         """
         Flange thickness, in.
         """
         return self.properties[AISC_FLANGE_THICKNESS]
 
     @property
-    def flange_width(self):
+    def flange_width(self) -> float:
         """
         Flange width, in.
         """
         return self.properties[AISC_FLANGE_WIDTH]
 
     @property
-    def j(self):
+    def j(self) -> float:
         """
         Torsional moment of inertia, in^4
         """
         return self.properties[AISC_J]
 
     @property
-    def k_design(self):
+    def k_design(self) -> float:
         """
         Design distance from outer face of flange to web toe of fillet, in.
         """
         return self.properties[AISC_K_DESIGN]
 
     @property
-    def web_thickness(self):
+    def web_thickness(self) -> float:
         """
         Web thickness, in.
         """
         return self.properties[AISC_WEB_THICKNESS]
 
     @property
-    def to_centroid_x(self):
+    def to_centroid_x(self) -> float:
         """
         Horizontal distance from designated member edge, as defined in the AISC Steel Construction Manual, to member
         centroidal axis, in.
@@ -101,7 +101,7 @@ class ChannelShape(RolledShape):
         return self.properties[AISC_TO_CENTROID_X]
 
     @property
-    def to_plastic_neutral_axis_x(self):
+    def to_plastic_neutral_axis_x(self) -> float:
         """
         Horizontal distance from designated member edge, as defined in the AISC Steel Construction Manual, to member
         plastic neutral axis, in.
@@ -109,7 +109,7 @@ class ChannelShape(RolledShape):
         return self.properties[AISC_TO_PLASTIC_NUETRAL_AXIS_X]
 
     @property
-    def to_shear_center(self):
+    def to_shear_center(self) -> float:
         """
         Horizontal distance from designated member edge, as defined in the AISC Steel Construction Manual, to member
         shear center, in.
